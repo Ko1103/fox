418d4fb7b3a8d7f9b7716dd8bf74ed6a7c46df60
KO-YAMAKO <ko.foot1103@gmail.com>
2019-01-16 14:58:40 +0900

    add mysql

--- soil/map/views.py
+++ soil/map/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.http import Http404
 
 def index(request):
-    return HttpResponse("Hello world.")
+    # template = loader.get_template('map/index.html')
+    # return HttpResponse(template)
+    return Http404('This page is not exist sorry')
--- soil/soil/settings.py
+++ soil/soil/settings.py
@@ -73,13 +73,22 @@ WSGI_APPLICATION = 'soil.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'soil', # database name
+        'USER': 'root',
+        'HOST': '',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
