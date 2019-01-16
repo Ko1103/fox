bfce844ec24b0e03ca8a398ceb2b850f2f397e19
KO-YAMAKO <ko.foot1103@gmail.com>
2019-01-16 15:51:10 +0900

    update connect django and mysql

--- soil/manage.py
+++ soil/manage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
 import sys
+import pymysql
 
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'soil.settings')
@@ -13,3 +14,6 @@ if __name__ == '__main__':
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+
+## setup mysql
+pymysql.install_as_MySQLdb()
