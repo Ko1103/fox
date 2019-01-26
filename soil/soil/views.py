355cd087be206f34912d1f5f7319b48bb3d21465
KO-YAMAKO <ko.foot1103@gmail.com>
2019-01-26 17:33:52 +0900

    setting viewsets

---
+++ soil/soil/views.py
@@ -0,0 +1,12 @@
+import django_filters
+from rest_framework import viewsets, filters
+from .models import Sensor, Reference
+from .serializer import SensorSerializer, ReferenceSerializer
+
+class SensorViewset(viewsets.ModelViewSet):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+class ReferenceViewset(viewsets.ModelViewSet):
+    queryset = Reference.objects.all()
+    serializer_class = ReferenceSerializer
