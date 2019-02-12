0df3a58a5aee9affa3c54ba586b0cd94b2fd0be9
KO-YAMAKO <ko.foot1103@gmail.com>
2019-02-12 17:22:48 +0900

    update system

--- map/mapping/views.py
+++ map/mapping/views.py
@@ -4,6 +4,7 @@ from django.template import Context, loader
 import json
 from django.views.decorators.csrf import ensure_csrf_cookie
 import requests
+import math
 
 def index(request):
     baseURL = 'https://backend.sigfox.com/api/v2/'
@@ -16,14 +17,14 @@ def index(request):
     if r.status_code == 200:
         data = r.json()
         app_data = data['data']
-    targetData = app_data[-1]
+    targetData = app_data[0]
     messageData = targetData['data']
     values = []
     for i in range(1,18,4):
         target = messageData[i:(i+3)]
         value = int(target, 16)
+        value = culcMC(value)
         values.append(value)
-    print(values)
     return render(request, 'mapping/index.html', {'data' : values})
 
 def sigfox(request):
@@ -51,3 +52,11 @@ def sigfox(request):
         return render(request, 'mapping/message.html', {'list' : app_data})
     return HttpResponse('Sorry U Failed')
 
+def culcMC(x): #含水比の計算
+    exp = math.e
+    ans = x + (6.54 * 1000)
+    ans = ans / (2.28 * 1000)
+    ans = ans * ans
+    ans = pow(exp, (ans * (-1)))
+    ans = 3.59 * 100000 * ans
+    return round(ans,1)
